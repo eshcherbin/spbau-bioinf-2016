9c414864273fe24518627b3604f6ab31fc60cf81
Olga Chernikova <Oche24912@yandex.ru>
2016-03-19 16:37:17 +0300

    add Makefile

--- tasks/insert_dist_graph/insert_dist.cpp
+++ tasks/insert_dist_graph/insert_dist.cpp
@@ -9,6 +9,11 @@ int main(int argc, char **argv) {
     char *fn = argv[1];
     samFile *fp = sam_open(fn, "r");
 
+    int delta = 1;
+    if (argc >= 3) {
+        delta = atoi(argv[2]);
+    }
+
     bam_hdr_t *sam_hdr = sam_hdr_read(fp);
 
     bam1_t* read = bam_init1();
@@ -23,8 +28,19 @@ int main(int argc, char **argv) {
 
     sort(reads_dist.begin(), reads_dist.end());
 
+    int max_pos = reads_dist[reads_dist.size() - 1];
+    int dist_count[max_pos/delta + 1];
+
+    for (int i = 0; i < max_pos/delta + 1; ++i) {
+        dist_count[i] = 0;
+    }
+
     for (int i = 0; i < (int)reads_dist.size(); ++i) {
-        printf("%d ", reads_dist[i]);
+        ++dist_count[reads_dist[i]/delta];
+    }
+
+    for (int i = 0; i <= max_pos/delta; ++i) {
+        printf("%d %d\n", i*delta, dist_count[i]);
     }
 
     sam_close(fp);
