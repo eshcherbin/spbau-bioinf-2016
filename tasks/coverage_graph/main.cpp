140b748bfbbff9f3a0ff1c431ad83cdab2dad96f
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-03-20 05:54:35 +0600

    several references and intron skipping supported

--- tasks/coverage_graph/main.cpp
+++ tasks/coverage_graph/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 const int DEFAULT_GRAPH_STEP_SIZE = 1000;
 
-int len, *cnt, step_size;
+int **cnt, step_size;
 char *fn;
 
 samFile *fp;
@@ -41,54 +41,81 @@ void init()
     fp = sam_open(fn, "r");
     hdr = sam_hdr_read(fp);
 
-    len = hdr->target_len[0];
-    cnt = new int[len];
-    fill(cnt, cnt + len, 0);
+    cnt = new int*[hdr->n_targets];
+    for (int tg = 0; tg < hdr->n_targets; tg++)
+    {
+        cnt[tg] = new int[hdr->target_len[tg]];
+        fill(cnt[tg], cnt[tg] + hdr->target_len[tg], 0);
+    }
 
     record = bam_init1();
 }
 
 void proc_record()
 {
-    for (int i = record->core.pos; i < bam_endpos(record); i++)
-        cnt[i]++;
+    if (record->core.flag & BAM_FUNMAP) 
+        return;
+    int pos = record->core.pos;
+    for (int i = 0; i < record->core.n_cigar; i++)
+    {
+        uint32_t c = bam_get_cigar(record)[i];
+        if (bam_cigar_type(c) & 1)
+        {
+            if (bam_cigar_type(c) & 2)
+            {
+                for (int j = 0; j < (int) bam_cigar_oplen(c); j++)
+                    cnt[record->core.tid][pos++]++;
+            }
+            else
+                pos += bam_cigar_oplen(c);
+        }
+    }
 }
 
 void output_stats()
 {
-    int max_cvg = 0;
-    double cvg = 0, avg = 0;
-
-    for (int i = 0; i < len; i++)
+    for (int tg = 0; tg < hdr->n_targets; tg++)
     {
-        if (cnt[i] > max_cvg)
-            max_cvg = cnt[i];
-        avg += cnt[i];
-        if (cnt[i])
-            cvg += 1;
-    }
-    avg /= len;
-    cvg /= len;
+        string fn = "plot_" + string(hdr->target_name[tg]) + ".dat";
+        ofstream data(fn.c_str());
+        printf("reference sequence %s:\n", hdr->target_name[tg]);
+        data << hdr->target_name[tg] << '\n';
 
-    printf("maximal coverage: %d reads\n", max_cvg);
-    printf("average coverage: %.2f reads/position\n", avg);
-    printf("%.2f%% positions covered\n", cvg * 100.);
+        int max_cvg = 0;
+        double cvg = 0, avg = 0;
 
-    ofstream data("plot.dat");
-    data << max_cvg << ' ' << fixed << setprecision(2) << avg << ' ' << cvg << '\n';
-    for (int l = 0; l < len; l += step_size)
-    {
-        int r = min(l + step_size, len);
-        double avg_step = 0;
-        for (int i = l; i < r; i++)
-            avg_step += cnt[i];
-        avg_step /= (r - l);
-        data << l << ' ' << fixed << setprecision(5) << avg_step << '\n';
+        for (int i = 0; i < (int) hdr->target_len[tg]; i++)
+        {
+            if (cnt[tg][i] > max_cvg)
+                max_cvg = cnt[tg][i];
+            avg += cnt[tg][i];
+            if (cnt[tg][i])
+                cvg += 1;
+        }
+        avg /= hdr->target_len[tg];
+        cvg /= hdr->target_len[tg];
+
+        printf("maximal coverage: %d reads\n", max_cvg);
+        printf("average coverage: %.2f reads/position\n", avg);
+        printf("%.2f%% positions covered\n", cvg * 100.);
+
+        data << max_cvg << ' ' << fixed << setprecision(2) << avg << ' ' << cvg << '\n';
+        for (int l = 0; l < (int) hdr->target_len[tg]; l += step_size)
+        {
+            int r = min(l + step_size, (int) hdr->target_len[tg]);
+            double avg_step = 0;
+            for (int i = l; i < r; i++)
+                avg_step += cnt[tg][i];
+            avg_step /= (r - l);
+            data << l << ' ' << fixed << setprecision(5) << avg_step << '\n';
+        }
     }
 }
 
 void destroy()
 {
+    for (int tg = 0; tg < hdr->n_targets; tg++)
+        delete [] cnt[tg];
     delete [] cnt;
     bam_destroy1(record);
     bam_hdr_destroy(hdr);
--- tasks/coverage_graph/plot.py
+++ tasks/coverage_graph/plot.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python
-
 from numpy import *
 from matplotlib.pyplot import *
+import sys
 
-with open('plot.dat') as f:
-    max_cvg, avg, cvg = map(float, f.readline().split())
-    max_cvg = int(max_cvg)
-    x, y = zip(*map(lambda s: tuple(map(float, s.split())), f.readlines()))
 
-figure()
+def main():
+    if len(sys.argv) == 1:
+        print('no file specified')
+        sys.exit(0)
 
-plot(x, y)
-grid(True)
+    with open(sys.argv[1]) as f:
+        ref_name = f.readline().strip()
+        max_cvg, avg, cvg = map(float, f.readline().split())
+        max_cvg = int(max_cvg)
+        x, y = zip(*map(lambda s: tuple(map(float, s.split())), f.readlines()))
 
-subplots_adjust(top=0.8, bottom=0.1)
+    figure()
 
-figtext(0.1, 0.95, 'maximal coverage: %d reads' % max_cvg)
-figtext(0.1, 0.9, 'average coverage: %.2f reads/position' % avg)
-figtext(0.1, 0.85, '%.2f%% positions covered' % (cvg * 100.))
+    title(ref_name)
+    plot(x, y)
+    grid(True)
 
-savefig('plot.png')
+    subplots_adjust(top=0.8, bottom=0.1)
+
+    figtext(0.1, 0.95, 'maximal coverage: %d reads' % max_cvg)
+    figtext(0.1, 0.9, 'average coverage: %.2f reads/position' % avg)
+    figtext(0.1, 0.85, '%.2f%% positions covered' % (cvg * 100.))
+
+    savefig('plot_' + ref_name + '.png')
+
+
+if __name__ == '__main__':
+    main()
