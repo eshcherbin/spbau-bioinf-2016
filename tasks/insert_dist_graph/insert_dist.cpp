2b271c69b85ccc1873a2ba5ac312b7d7fdf93e43
Olga Chernikova <Oche24912@yandex.ru>
2016-03-20 12:31:50 +0300

    add average

--- tasks/insert_dist_graph/insert_dist.cpp
+++ tasks/insert_dist_graph/insert_dist.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -48,6 +49,54 @@ public:
         }
     }
 
+    int getMedDist() {
+        int start_pos = 0, end_pos = (int)dist_count.size() - 1;
+        int cur_val1 = dist_count[start_pos], cur_val2 = dist_count[end_pos];
+        while (start_pos != end_pos) {
+	    if (cur_val1 > cur_val2) {
+	        cur_val1 -= cur_val2;
+		--end_pos;
+		cur_val2 = dist_count[end_pos];
+	    } else {
+	        cur_val2 -= cur_val1;
+		++start_pos;
+		cur_val1 = dist_count[start_pos];
+	    }
+	}
+	return start_pos * delta;	
+    }
+
+    int getAverage() {
+        int sum = 0;
+	int cnt = 0;
+	for (int i = 0; i < (int)dist_count.size(); ++i) {
+	    sum += dist_count[i] * i * delta;
+	    cnt += dist_count[i];
+	}
+	return sum/cnt;
+    }
+
+    int getDeviation() {
+        int aver = getAverage();
+	long long sum = 0;
+	int cnt = 0;
+	for (int i = 0; i < (int)dist_count.size(); ++i) {
+	    cnt += dist_count[i];
+	    sum += (i*delta - aver)*(long long)(i*delta - aver)*dist_count[i];
+	}
+	return sqrt((double)sum/cnt);
+    }
+
+    void cutPartOfSegment() {
+        int dev = getDeviation();
+	int med = getMedDist();
+	int sp = (med - 5*dev)/delta, ep = (med + 5*dev)/delta;
+	for (int i = 0; i < sp; ++i) {
+	    dist_count[i] = 0;
+	}
+	dist_count.resize(ep, 0);
+    }
+
     ~InsertDist() {
        sam_close(fp);
     }
@@ -62,8 +111,15 @@ int main(int argc, char **argv) {
     }
 
     InsertDist id = InsertDist(fn, delta);
+    for (int i = 0; i < 5; ++i) {
+        id.cutPartOfSegment();
+    }
+
     id.printDistCount();
 
+    cerr << "MedDist: " << id.getMedDist() << endl;
+    cerr << "Average: " << id.getAverage()  << endl;
+    cerr << "Deviation: " << id.getDeviation() << endl;
     return 0;
 }
 
